027042f12cda1eadbe6ac80ddad06a88e096c1c8
ChawinSS <137822600+ChawinSS@users.noreply.github.com>
2025-06-24 21:01:38 +0200

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,62 @@
+const quizDataFromFile = [
+  {
+    "question": "immer",
+    "correct": "always",
+    "choices": [
+      "never",
+      "not",
+      "very",
+      "always"
+    ]
+  },
+  {
+    "question": "oft",
+    "correct": "often",
+    "choices": [
+      "maybe",
+      "often",
+      "never",
+      "very"
+    ]
+  },
+  {
+    "question": "nicht",
+    "correct": "not",
+    "choices": [
+      "not",
+      "maybe",
+      "never",
+      "often"
+    ]
+  },
+  {
+    "question": "nie",
+    "correct": "never",
+    "choices": [
+      "never",
+      "maybe",
+      "very",
+      "always"
+    ]
+  },
+  {
+    "question": "sehr",
+    "correct": "very",
+    "choices": [
+      "not",
+      "always",
+      "often",
+      "very"
+    ]
+  },
+  {
+    "question": "vielleicht",
+    "correct": "maybe",
+    "choices": [
+      "not",
+      "maybe",
+      "often",
+      "very"
+    ]
+  }
+];
